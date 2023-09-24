aa8ad6a39a4c69bb35f09588a026b5b9c0f01c25
SongSeungJu <wkwjsrj5079@gmail.com>
2023-09-24 15:38:09 +0900

    {23.09.23} 처음 스프링 부트 빌딩

---
+++ s0613/src/main/java/com/example/s0613/S0613Application.java
@@ -0,0 +1,13 @@
+package com.example.s0613;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S0613Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S0613Application.class, args);
+    }
+
+}
---
+++ s0613/src/test/java/com/example/s0613/S0613ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s0613;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S0613ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
